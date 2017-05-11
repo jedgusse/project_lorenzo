164629b603a2ae2e0c67a27c18663fcd6cc375fa
emanjavacas <enrique.manjavacas@gmail.com>
2017-05-11 21:47:45 +0200

    WIP augmentation.py

--- src/augmentation.py
+++ src/augmentation.py
@@ -3,49 +3,54 @@
 from pprint import pprint
 import os
 import argparse
+import multiprocessing
+
+from joblib import Parallel, delayed
 
 from sklearn.metrics import classification_report
 from sklearn import preprocessing
 
+from seqmod.utils import load_model
+
+from utils import generate_docs, docs_to_X
 from data import DataReader
-from generator import LMGenerator
-from classifier import pipe_grid_clf, docs_to_X
+from classifier import pipe_grid_clf
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('path')
     parser.add_argument('--load_generated', action='store_true')
-    parser.add_argument('--save_generated', action='store_true')                        
+    parser.add_argument('--save_generated', action='store_true')
     parser.add_argument('--generated_path',
-                        help='Path to generated files. Filename ' + \
+                        help='Path to generated files. Filename ' +
                         'in format: authorname_docnum.txt')
     parser.add_argument('--nb_docs', default=10, type=int,
                         help='Number of generated docs per author')
     args = parser.parse_args()
 
-    reader = DataReader.load(args.reader_path)
+    # - Load reader and generator paths
+    reader_path, generators = None, {}
+    for f in os.listdir(args.path):
+        if f.endswith('pkl'):   # reader path
+            reader_path = os.path.join(args.path, f)
+        elif f.endswith('pt'):  # generator path
+            basename = os.path.splitext(os.path.basename(f))[0]
+            author = ' '.join(basename.split('_'))
+            generators[author] = os.path.join(args.path, f)
+
+    if args.reader_path:        # load reader from custom path
+        reader_path = args.reader_path
+
+    if not reader_path:
+        raise ValueError("Couldn't find reader in dir [%s]" % args.path)
+
+    reader = DataReader.load(reader_path)
     gener, discrim, test = reader.foreground_splits()
 
     X_train, y_train, X_test, y_test = [], [], [], []
 
     # - Generate (or load) generated documents
-    def generate_docs(generator, author, nb_docs, max_words,
-                      save=False, path=None):
-        print("::: Generating %d docs for %s" % (args.nb_docs, author))
-        docs, scores = [], []
-        max_sent_len = max(len(s) for s in generator.examples)
-        for doc_id in range(nb_docs):
-            doc, score = generator.generate_doc(
-                max_words=max_words, max_sent_len=max_sent_len)
-            docs.append(doc), scores.append(score)
-            if save:
-                doc_id = str(doc_id + 1).rjust(len(str(nb_docs)), '0')
-                fname = '%s.%s.txt' % ('_'.join(author.split()), doc_id)
-                with open(os.path.join(path, fname), 'w+') as f:
-                    f.write('\n'.join(doc))
-        return docs, author
-
     if args.load_generated:     # load generated documents
         assert not args.generated_path or os.path.isdir(args.generated_path), \
             "argument to --generated_path is not a valid path"
@@ -66,10 +71,12 @@ if __name__ == '__main__':
                 save=args.save_generated, path=args.generated_path)
             for author, fpath in generators.items())
         for docs, author in results:
-            X_train.extend(docs), y_train.extend([author for _ in range(len(docs))])
+            X_train.extend(docs)
+            y_train.extend([author for _ in range(len(docs))])
 
     # - Load real data (for testing)
-    # TODO
+    _, discrim, _ = reader.foreground_splits()
+    _, X_test, y_test = discrim
 
     le = preprocessing.LabelEncoder()
     y_train = le.fit_transform(y_train)
