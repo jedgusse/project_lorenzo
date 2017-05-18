2534ed901e6299feeee6d2d15a69e46568509c54
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-05-18 19:59:08 +0200

    added trajectory computation and parallelization

--- src/lexical_richness.py
+++ src/lexical_richness.py
@@ -11,13 +11,17 @@ independent of text length inside their respective family.
 import math
 from collections import Counter, defaultdict
 
+import joblib
+import multiprocessing
 import numpy as np
 
 
-def permute_corpus(corpus):
+def permute_corpus(corpus, rng=None):
     """
     Randomly permutes a corpus in word-form [w, w, w, ...]
     """
+    if rng is not None:
+        return rng.permutation(np.array(corpus))
     return np.random.permutation(np.array(corpus))
 
 
@@ -65,10 +69,44 @@ def K(corpus):
     return 10e3 * (-1/len(corpus) + term)
 
 
-def trajectory(corpus, func, n_chunks):
+def trajectory(corpus, func, n_chunks, rng=None):
     """
     Computes lexical trajectory over steps following partition by `n_chunks`
     """
-    perm = permute_corpus(corpus)
+    perm = permute_corpus(corpus, rng)
     step = len(corpus) // n_chunks
     return [(i, func(perm[:i])) for i in list(range(0, len(corpus), step))[1:]]
+
+
+def mc_interval(samples, interval=0.025):
+    """
+    Monte Carlo confidence interval on an input vector of samples.
+    Returns lower, mean and upper values for the interval.
+    """
+    samples = np.array(samples)
+    split_idx = int(len(samples) * interval)
+    sort = np.sort(samples)
+    return sort[split_idx], sort.mean(), sort[-split_idx]
+
+
+def bootstrap_trajectory(corpus, func, n_chunks, resampling=1000):
+    """
+    Computes a `resampling` number of samples for the given richness
+    `func` at each of the steps resulting from chunking `corpus` in
+    `n_chunks` chunks.
+
+    Return
+    ------
+    [(step, (score_1, score_2, score_resampling)), ...]
+    """
+    seeds = [r * np.random.randint(10) for r in range(resampling)]
+    # compute bootstrapped values
+    result = [trajectory(corpus, func, n_chunks) for _ in range(resampling)]
+    num_cores = multiprocessing.cpu_count()
+    result = joblib.Parallel(n_jobs=num_cores)(
+        joblib.delayed(trajectory)(
+            corpus, func, n_chunks, rng=np.random.RandomState(seeds[r])
+        ) for r in range(resampling))
+    # compute confidence intervals
+    return [(step[0][0], mc_interval([score for _, score in step]))
+            for step in zip(*result)]
